ae961041a903169347ea2add8ee7a0919feae22a
rishivyas1969 <rishivyas2424@gmail.com>
2021-09-14 20:16:49 +0530

    Ready

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import cv2, time, numpy
+from datetime import datetime
+import pandas
+
+df = pandas.DataFrame(columns=['Entry', 'Exit'])
 
 first_frame = None
 video = cv2.VideoCapture(0)
 
+status_list = [None, None]
+times = []
+
 while True:
     check, frame = video.read()
+
+    status = 0
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
@@ -21,8 +30,19 @@ while True:
     for contours in cnts:
         if cv2.contourArea(contours) < 1000 :
             continue
+
+        status = 1
         (x, y, w, h) = cv2.boundingRect(contours)
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
+
+    status_list.append(status)
+    if status_list[-1] == 0 and status_list[-2] == 1:
+        entry_time = datetime.now()
+    
+    if status_list[-1] == 0 and status_list[-2] == 1:
+        exit_time = datetime.now()
+        times.append([entry_time, exit_time])
+
 
     cv2.imshow('Color Frame', frame)
     cv2.imshow('Delta Frame', delta_frame)
@@ -32,7 +52,14 @@ while True:
     key = cv2.waitKey(1)
 
     if key == ord('m'):
+        if status == 1:
+            times.append([entry_time, datetime.now()])
         break
+
+for i in times:
+    df = df.append({'Entry': i[0], 'Exit': i[1]}, ignore_index=True)
+
+df.to_csv('Times.csv')
 
 cv2.destroyAllWindows()
 
